4aebc54acd0cddce531d2ce6314e5692a91e252e
Yagami <ayrtonyagami@hotmail.com>
2020-08-14 18:44:24 +0100

    primeira tele

---
+++ AppHoje/Adapter/HojeAdapter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using Android.Views;
+using Android.Widget;
+using System.Collections.Generic;
+using AppHoje.DataModel;
+using Android.Support.V7.Widget;
+
+namespace AppHoje.Adapter
+{
+    class HojeAdapter : RecyclerView.Adapter
+    {
+        public event EventHandler<HojeAdapterClickEventArgs> ItemClick;
+        public event EventHandler<HojeAdapterClickEventArgs> ItemLongClick;
+        List<Prato> items;
+
+        public HojeAdapter(List<Prato> data)
+        {
+            items = data;
+        }
+
+        // Create new views (invoked by the layout manager)
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+
+            //Setup your layout here
+            View itemView = null;
+            var id = Resource.Layout.hojerow;
+            itemView = LayoutInflater.From(parent.Context).
+                   Inflate(id, parent, false);
+
+            var vh = new HojeAdapterViewHolder(itemView, OnClick, OnLongClick);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            var item = items[position];
+
+            // Replace the contents of the view with that element
+            var holder = viewHolder as HojeAdapterViewHolder;
+            //holder.TextView.Text = items[position];
+        }
+
+        public override int ItemCount => items.Count;
+
+        void OnClick(HojeAdapterClickEventArgs args) => ItemClick?.Invoke(this, args);
+        void OnLongClick(HojeAdapterClickEventArgs args) => ItemLongClick?.Invoke(this, args);
+
+    }
+
+    public class HojeAdapterViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView txtNome { get; set; }
+        public TextView txtDepartamento { get; set; }
+        public TextView txtTexto { get; set; }
+        public TextView txtStatus { get; set; }
+        public ImageView imgDelete { get; set; }
+
+
+        public HojeAdapterViewHolder(View itemView, Action<HojeAdapterClickEventArgs> clickListener,
+                            Action<HojeAdapterClickEventArgs> longClickListener) : base(itemView)
+        {
+            //TextView = v;
+            txtNome = (TextView)itemView.FindViewById(Resource.Id.txtNome);
+            txtDepartamento = (TextView)itemView.FindViewById(Resource.Id.txtDepartamento);
+            txtTexto = (TextView)itemView.FindViewById(Resource.Id.txtTexto);
+            txtStatus = (TextView)itemView.FindViewById(Resource.Id.txtStatus);
+            imgDelete = (ImageView)itemView.FindViewById(Resource.Id.imgDelete);
+
+            itemView.Click += (sender, e) => clickListener(new HojeAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+            itemView.LongClick += (sender, e) => longClickListener(new HojeAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+        }
+    }
+
+    public class HojeAdapterClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
---
+++ AppHoje/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System;
+
+namespace AppHoje
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText myEditText;
+        ImageView btnAdicionar;
+        ImageView btnBuscar;
+        RecyclerView myRecyclerView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            myEditText = (EditText)FindViewById(Resource.Id.txtBuscar);
+            btnAdicionar = (ImageView)FindViewById(Resource.Id.btnNovo);
+            btnBuscar = (ImageView)FindViewById(Resource.Id.btnProcurar);
+            myRecyclerView = (RecyclerView)FindViewById(Resource.Id.recyclerView1);
+
+            btnBuscar.Click += BtnBuscar_Click;
+
+
+        }
+
+        private void BtnBuscar_Click(object sender, System.EventArgs e)
+        {
+            if(myEditText.Visibility == Android.Views.ViewStates.Gone)
+            {
+                myEditText.Visibility = Android.Views.ViewStates.Visible;
+            }
+            else
+            {
+                myEditText.ClearFocus();
+                myEditText.Visibility = Android.Views.ViewStates.Gone;
+
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
